a9c2342eb444ed188ce9ab1145a0b046abba92b1
maitrimishra13 <delhi.maitri@gmail.com>
2025-11-27 11:29:16 +0530

    Assignment 3 - Stacks - (Postfix Infix = yet to understand completely!)

--- src/assignment6/Question1.java
+++ src/assignment6/Question1.java
@@ -1,4 +1,43 @@
 package assignment6;
 
 public class Question1 {
+    public static class DoubleLinkedList{
+        private static class Node{
+            int data;
+            Node next;
+            Node prev;
+            Node(int val) {
+                data = val;
+                prev = null;
+                next = null;
+            }
+        }
+
+        private Node head;
+
+        public DoubleLinkedList() {
+            head = null;
+        }
+
+        public void search(int num){
+            if(head == null){
+                System.out.println("List is empty");
+            }
+            Node cur = head;
+            while(cur != null){
+                if(cur.data == num){
+                    System.out.println("Element found");
+                    return;
+                }
+                cur = cur.next;
+            }
+            System.out.println("Element not found");
+        }
+    }
+
+    public static void main(String[] args) {
+        DoubleLinkedList list = new DoubleLinkedList();
+        list.search(1);
+        list.search(2);
+    }
 }
